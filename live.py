ebe1b6758e611060c0a6f6dbb8535b90c41118b6
tdinkov <tdinkov@cisco.com>
2024-09-14 10:52:52 +0300

    Refactoring

--- live.py
+++ live.py
@@ -2,55 +2,38 @@ def welcome(name):
     print(f'Hello {name} and welcome to the World of Games (WoG).\nHere you can find many cool games to play.\n')
 
 
+def get_valid_input(prompt, valid_range):
+    valid_range_list = list(valid_range)
+    while True:
+        try:
+            choice = int(input(prompt))
+            if choice in valid_range_list:
+                return choice
+            else:
+                valid_values = ', '.join(map(str, valid_range_list))
+                print(f"Please enter a valid number from: {valid_values}.")
+        except ValueError:
+            print('Invalid input. Please enter a valid integer.')
+
+
 def load_game():
-    print(f'Please choose a game to play:\n'
-          '     1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back\n'
-          '     2. Guess Game - guess a number and see if you chose like the computer\n'
-          '     3. Currency Roulette - try and guess the value of a random amount of USD in ILS\n')
+    games = {
+        1: 'Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back',
+        2: 'Guess Game - guess a number and see if you chose like the computer',
+        3: 'Currency Roulette - try and guess the value of a random amount of USD in ILS'
+    }
+    print(f'Please choose a game to play: \n')
+    for key, description in games.items():
+        print(f'    {key}. {description}')
 
-    while True:
-        try:
-            choice = int(input('Please make your choice: '))
-            if choice == 1:
-                print('\nWelcome to Memory Game!')
-                break
-            elif choice == 2:
-                print('\nWelcome to Guess Game!')
-                break
-            elif choice == 3:
-                print('\nWelcome to Currency Roulette!')
-                break
-            else:
-                print('Please enter a number from 1 to 3!')
-        except ValueError:
-            print('Invalid input. Please enter a valid integer.')
+    game_choice = get_valid_input('\nPlease make your choice: ', games.keys())
 
-    print('\nPlease choose game difficulty from 1 to 5: \n'
-          '     1. Beginner\n'
-          '     2. Easy\n'
-          '     3. Normal\n'
-          '     4. Hard\n'
-          '     5. Insane\n')
+    print(f'\nWelcome to {games[game_choice].split(" - ")[0]}!')
 
-    while True:
-        try:
-            difficulty = int(input('Please select your preferred difficulty level: '))
-            if difficulty == 1:
-                print('\nYour preference is to play as Beginner\n')
-                break
-            elif difficulty == 2:
-                print('\nYour preference is to play as Easy\n')
-                break
-            elif difficulty == 3:
-                print('\nYour preference is to play as Normal\n')
-                break
-            elif difficulty == 4:
-                print('\nYour preference is to play as Hard\n')
-                break
-            elif difficulty == 5:
-                print('\nYour preference is to play as Insane\n')
-                break
-            else:
-                print('\nPlease select a number from 1 to 5\n')
-        except ValueError:
-            print('Invalid input. Please enter a valid integer.')
+    difficulties = ['Beginner', 'Easy', 'Normal', 'Hard', 'Insane']
+    print('\nPlease choose game difficulty from 1 to 5: \n')
+    for idx, difficulty in enumerate(difficulties, 1):
+        print(f'    {idx}. {difficulty}')
+
+    difficulty_choice = get_valid_input('\nPlease select your preferred difficulty level: ', range(1, 6))
+    print(f'\nYour preference is to play as {difficulties[difficulty_choice - 1]}\n')
